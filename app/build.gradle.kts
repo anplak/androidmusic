b5bca8c90341c06dc9e4201b632e02d7fdba002e
Aleksei Plakhotnyi <alex.plakhotnyi@assaabloy.com>
2026-08-17 16:28:02 +0200

    build issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,34 @@ plugins {
     alias(libs.plugins.detekt)
 }
 
+import java.io.File
+import java.util.Properties
+
+fun Properties.loadFromLocalProperties(propName: String): String? {
+    return this[propName]?.toString()?.trim()
+}
+
+fun getKeystorePassword(): String {
+    val localProps = Properties().apply {
+        File("local.properties").inputStream().use { load(it) }
+    }
+    return localProps.loadFromLocalProperties("keystore.password") ?: "androidmusic"
+}
+
+fun getKeystoreAlias(): String {
+    val localProps = Properties().apply {
+        File("local.properties").inputStream().use { load(it) }
+    }
+    return localProps.loadFromLocalProperties("keystore.alias") ?: "androidmusic"
+}
+
+fun getKeystoreKeyPassword(): String {
+    val localProps = Properties().apply {
+        File("local.properties").inputStream().use { load(it) }
+    }
+    return localProps.loadFromLocalProperties("keystore.key.password") ?: "androidmusic"
+}
+
 android {
     namespace = "com.anplak.androidmusic"
     compileSdk = 34
@@ -15,7 +43,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -24,13 +52,24 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystores/androidmusic.keystore")
+            storePassword = getKeystorePassword()
+            keyAlias = getKeystoreAlias()
+            keyPassword = getKeystoreKeyPassword()
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
